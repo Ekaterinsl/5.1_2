fd1d15a278bea744f193c9d6bbbe82d4c11809e3
Apelsin <Apelsin@DESKTOP-I9KKGKE>
2022-08-21 16:10:00 +0400

    Добавьте файлы проекта.

---
+++ 5.1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._1_2
+{
+    internal class Program
+    {
+        static string DivideWord(string s)
+        {
+            string[] subs = s.Split(' ');
+
+
+            foreach (var sub in subs)
+            {
+                Console.WriteLine($" {sub}");
+            }
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+
+            DivideWord(text);
+        }
+    }
+}
